3aedfdb7c0634ca436202f631e8b1f7c0e81b5e7
ruben <ruben@Mac.home>
2025-06-26 12:01:02 +0200

    first vite commit

--- src/pages/Skills/SkillCardItem.jsx
+++
@@ -1,9 +0,0 @@
-import './SkillCardItem.css';
-export const SkillCardItem = ({ label, color }) => {
-  return (
-    <span
-      style={{ border: `1px solid ${color}`, color: color, fontSize: "20px"}}
-      className="skillCardItem neo-5-out">{label}
-    </span>
-  );
-};
