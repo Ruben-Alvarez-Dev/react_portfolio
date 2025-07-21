379391d8974c8f22ea67ae5233fcef1aa5a48e49
ruben <ruben@Mini-de-ruben.home>
2025-07-22 01:08:14 +0200

    reset: starting from scratch to vite blank project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { ThemeProvider } from './contexts/ThemeProvider';
-import Header from './components/layout/Header';
-import Main from './components/layout/Main';
-import Footer from './components/layout/Footer';
-import Hero from './components/sections/Hero';
-import About from './components/sections/About';
-import Projects from './components/sections/Projects';
-import Contact from './components/sections/Contact';
-import './main.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider>
-      <div className="App">
-        <Header />
-        <Main>
-          <Hero />
-          <About />
-          <Projects />
-          <Contact />
-        </Main>
-        <Footer />
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </ThemeProvider>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
